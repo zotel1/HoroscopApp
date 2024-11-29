96f2c24c56f1ce1a374a58dfaf5da02d2ae63130
zotel1 <zotelsigel@gmail.com>
2024-11-28 22:13:56 -0300

    update, creamos nuestro tercer fragment PalmistryFragment

---
+++ app/src/main/java/com/zoteldev/horoscopapp/ui/luck/LuckFragment.kt
@@ -0,0 +1,22 @@
+package com.zoteldev.horoscopapp.ui.luck
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.zoteldev.horoscopapp.R
+import com.zoteldev.horoscopapp.databinding.FragmentLuckBinding
+
+class LuckFragment : Fragment() {
+    private var _binding: FragmentLuckBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentLuckBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+}
