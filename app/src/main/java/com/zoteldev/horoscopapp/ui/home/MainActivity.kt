133de468df77e9e41dd59ac12392daa415013c0d
zotel1 <zotelsigel@gmail.com>
2024-11-30 13:30:13 -0300

    update 1:07

--- app/src/main/java/com/zoteldev/horoscopapp/ui/home/MainActivity.kt
+++ app/src/main/java/com/zoteldev/horoscopapp/ui/home/MainActivity.kt
@@ -5,17 +5,31 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
 import com.zoteldev.horoscopapp.R
 import com.zoteldev.horoscopapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        initUI()
+    }
 
+    private fun initUI() {
+        initNavigation()
+    }
+
+    private fun initNavigation() {
+        val navHost : NavHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        navController = navHost.navController
     }
 }
