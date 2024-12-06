06b45b0140ee60ddcd90ffb0407b6bf8bb0c781f
zotel1 <zotelsigel@gmail.com>
2024-12-05 22:25:46 -0300

    update, add objects HoroscopeInfo

--- app/src/main/java/com/zoteldev/horoscopapp/domain/model/HoroscopeInfo.kt
+++ app/src/main/java/com/zoteldev/horoscopapp/domain/model/HoroscopeInfo.kt
@@ -3,5 +3,16 @@ package com.zoteldev.horoscopapp.domain.model
 import com.zoteldev.horoscopapp.R
 
 sealed class HoroscopeInfo(  val img: Int, val nane: Int) {
-    object Aries: HoroscopeInfo(R.drawable.aries, R.string.app_name)
+    object Aries: HoroscopeInfo(R.drawable.aries, R.string.aries)
+    object Taurus: HoroscopeInfo(R.drawable.tauro, R.string.taurus)
+    object Gemini: HoroscopeInfo(R.drawable.geminis, R.string.gemini)
+    object Cancer: HoroscopeInfo(R.drawable.cancer, R.string.cancer)
+    object Leo: HoroscopeInfo(R.drawable.leo, R.string.leo)
+    object Virgo: HoroscopeInfo(R.drawable.virgo, R.string.virgo)
+    object Libra: HoroscopeInfo(R.drawable.libra, R.string.libra)
+    object Scorpio: HoroscopeInfo(R.drawable.escorpio, R.string.scorpio)
+    object sagittarius: HoroscopeInfo(R.drawable.sagitario, R.string.sagittarius)
+    object Capricorn: HoroscopeInfo(R.drawable.capricornio, R.string.capricorn)
+    object Aquarius: HoroscopeInfo(R.drawable.aquario, R.string.aquarius)
+    object Pisces: HoroscopeInfo(R.drawable.piscis, R.string.pisces)
 }
