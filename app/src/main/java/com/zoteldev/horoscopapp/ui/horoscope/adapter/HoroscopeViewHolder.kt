670c2f3af1a36ba35710ba7d89acbbeac971ad4c
zotel1 <zotelsigel@gmail.com>
2024-12-09 00:03:20 -0300

    update, add first animation 2:47

--- app/src/main/java/com/zoteldev/horoscopapp/ui/horoscope/adapter/HoroscopeViewHolder.kt
+++ app/src/main/java/com/zoteldev/horoscopapp/ui/horoscope/adapter/HoroscopeViewHolder.kt
@@ -1,6 +1,7 @@
 package com.zoteldev.horoscopapp.ui.horoscope.adapter
 
 import android.view.View
+import android.view.animation.LinearInterpolator
 import androidx.recyclerview.widget.RecyclerView
 import com.zoteldev.horoscopapp.databinding.ItemHoroscopeBinding
 import com.zoteldev.horoscopapp.domain.model.HoroscopeInfo
@@ -9,14 +10,26 @@ class HoroscopeViewHolder(view:View):RecyclerView.ViewHolder(view){
 
     private val binding = ItemHoroscopeBinding.bind(view)
 
-    fun render(horoscopeInfo: HoroscopeInfo, onItemSelected: (HoroscopeInfo) -> Unit){
+    fun render(horoscopeInfo: HoroscopeInfo, onItemSelected: (HoroscopeInfo) -> Unit) {
 
         val context = binding.tvTitle.context
 
         binding.ivHoroscope.setImageResource(horoscopeInfo.img)
         binding.tvTitle.text = context.getString(horoscopeInfo.nane)
 
-        binding.parent.setOnClickListener { onItemSelected(horoscopeInfo) }
+        binding.parent.setOnClickListener {
+            startRotationAnimation(binding.ivHoroscope)
+            //   onItemSelected(horoscopeInfo) }
 
+        }
+    }
+
+    private fun startRotationAnimation(view:View){
+        view.animate().apply {
+            duration = 500
+            interpolator = LinearInterpolator()
+            rotationBy(360f)
+            start()
+        }
     }
 }
