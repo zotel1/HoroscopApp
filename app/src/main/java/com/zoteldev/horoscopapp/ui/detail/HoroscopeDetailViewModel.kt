2d6e6cd6b59bb04ef1947c3d6954004e41a76d0d
zotel1 <zotelsigel@gmail.com>
2024-12-18 21:07:46 -0300

    update, mostramos nuestras vistas con nuestro uso de caso

--- app/src/main/java/com/zoteldev/horoscopapp/ui/detail/HoroscopeDetailViewModel.kt
+++ app/src/main/java/com/zoteldev/horoscopapp/ui/detail/HoroscopeDetailViewModel.kt
@@ -1,14 +1,27 @@
 package com.zoteldev.horoscopapp.ui.detail
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.zoteldev.horoscopapp.domain.usecase.GetPredictionUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 @HiltViewModel
-class HoroscopeDetailViewModel @Inject constructor():ViewModel() {
+class HoroscopeDetailViewModel @Inject constructor(private val getPredictionUseCase: GetPredictionUseCase):ViewModel() {
     private var _state = MutableStateFlow<HoroscopeDetailState>(HoroscopeDetailState.Loading)
 
     val state: StateFlow<HoroscopeDetailState> = _state
+
+    fun getHoroscope(sign:String){
+        viewModelScope.launch {
+            // Hilo principal
+            withContext(Dispatchers.IO) { getPredictionUseCase(sign) } // Hilo secundario
+            // Hilo principal
+        }
+    }
 }
