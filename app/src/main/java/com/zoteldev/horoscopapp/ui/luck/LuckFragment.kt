69ed926d8bfba86a543dbf5457a76bacb628bc00
zotel1 <zotelsigel@gmail.com>
2024-12-22 11:41:28 -0300

    update 5:39 add first animation

--- app/src/main/java/com/zoteldev/horoscopapp/ui/luck/LuckFragment.kt
+++ app/src/main/java/com/zoteldev/horoscopapp/ui/luck/LuckFragment.kt
@@ -1,13 +1,18 @@
 package com.zoteldev.horoscopapp.ui.luck
 
+import android.animation.ObjectAnimator
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.DecelerateInterpolator
+import androidx.core.animation.doOnEnd
 import com.zoteldev.horoscopapp.R
 import com.zoteldev.horoscopapp.databinding.FragmentLuckBinding
 import dagger.hilt.android.AndroidEntryPoint
+import java.util.Random
+
 
 @AndroidEntryPoint
 class LuckFragment : Fragment() {
@@ -15,6 +20,36 @@ class LuckFragment : Fragment() {
 
     private var _binding: FragmentLuckBinding? = null
     private val binding get() = _binding!!
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initUI()
+
+    }
+
+    private fun initUI(){
+        initListeners()
+    }
+
+    private fun initListeners(){
+        binding.ivRoulette.setOnClickListener { spinRoulette() }
+    }
+
+    private fun spinRoulette(){
+
+        val random = Random()
+        val degrees:Int = random.nextInt(1440) + 360
+
+        val animator = ObjectAnimator.ofFloat(binding.ivRoulette, View.ROTATION, 0f, degrees.toFloat())
+        animator.duration = 2000
+        animator.interpolator = DecelerateInterpolator()
+        animator.doOnEnd {  }
+    animator.start()
+    }
+
+    private fun slideCard(){
+
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
