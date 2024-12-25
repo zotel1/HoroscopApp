4696ae2206cd3121ba4d5db053d8ef83e1fbd101
zotel1 <zotelsigel@gmail.com>
2024-12-25 19:04:45 -0300

    update, 6:43 luck screen completed

--- app/src/main/java/com/zoteldev/horoscopapp/ui/luck/LuckFragment.kt
+++ app/src/main/java/com/zoteldev/horoscopapp/ui/luck/LuckFragment.kt
@@ -1,6 +1,7 @@
 package com.zoteldev.horoscopapp.ui.luck
 
 import android.animation.ObjectAnimator
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -15,6 +16,7 @@ import androidx.core.animation.doOnEnd
 import androidx.core.view.isVisible
 import com.zoteldev.horoscopapp.R
 import com.zoteldev.horoscopapp.databinding.FragmentLuckBinding
+import com.zoteldev.horoscopapp.ui.core.listeners.OnSwipeTouchListener
 import com.zoteldev.horoscopapp.ui.providers.RandomCardProvider
 import dagger.hilt.android.AndroidEntryPoint
 import java.util.Random
@@ -63,8 +65,19 @@ class LuckFragment : Fragment() {
         startActivity(shareIntent)
     }
 
+    @SuppressLint("ClickableViewAccessibility")
     private fun initListeners(){
-        binding.ivRoulette.setOnClickListener { spinRoulette() }
+      //  binding.ivRoulette.setOnClickListener { spinRoulette() }
+        binding.ivRoulette.setOnTouchListener(object : OnSwipeTouchListener(requireContext()){
+
+            override fun onSwipeRight() {
+                spinRoulette()
+            }
+
+            override fun onSwipeLeft() {
+                spinRoulette()
+            }
+        })
     }
 
     private fun spinRoulette(){
