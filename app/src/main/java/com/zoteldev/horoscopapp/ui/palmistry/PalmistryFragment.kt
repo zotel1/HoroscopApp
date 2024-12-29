9b4ac97e82ef5f4812df5162271c3e8dea49ad37
zotel1 <zotelsigel@gmail.com>
2024-12-29 17:21:16 -0300

    update, 7:07

--- app/src/main/java/com/zoteldev/horoscopapp/ui/palmistry/PalmistryFragment.kt
+++ app/src/main/java/com/zoteldev/horoscopapp/ui/palmistry/PalmistryFragment.kt
@@ -1,15 +1,18 @@
 package com.zoteldev.horoscopapp.ui.palmistry
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.core.content.ContextCompat
 import androidx.core.content.PermissionChecker
-import com.zoteldev.horoscopapp.Manifest
-import com.zoteldev.horoscopapp.R
 import com.zoteldev.horoscopapp.databinding.FragmentPalmistryBinding
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -53,9 +56,32 @@ class PalmistryFragment : Fragment() {
         }
     }
     private fun checkCameraPermission(): Boolean{
-        return PermissionChecker.checkPermission(
+        return PermissionChecker.checkSelfPermission(
             requireContext(), CAMERA_PERMISSION
         ) == PermissionChecker.PERMISSION_GRANTED
+    }
+
+    private fun startCamera(){
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(requireContext())
+        cameraProviderFuture.addListener({
+            val cameraProvider:ProcessCameraProvider = cameraProviderFuture.get()
+
+            val preview = Preview.Builder()
+                .build()
+                .also{
+                    it.setSurfaceProvider(binding.viewFinder.surfaceProvider)
+                }
+
+            val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+
+            try {
+                cameraProvider.unbindAll()
+
+                cameraProvider.bindToLifecycle(this, cameraSelector, preview)
+            } catch (e:Exception){
+                Log.e("zotel", "Algo salio mal ${e.message}")
+            }
+        }, ContextCompat.getMainExecutor(requireContext()))
     }
 
 }
